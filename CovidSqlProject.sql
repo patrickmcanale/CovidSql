6f19f13cd20462fe76e07d42feb9a5a032d86cb9
Pat Canale <73977982+patrickmcanale@users.noreply.github.com>
2021-09-25 09:05:21 -0500

    Update CovidSqlProject.sql
    commit change2

--- CovidSqlProject.sql
+++ CovidSqlProject.sql
@@ -17,7 +17,10 @@ Where continent is not null
 order by 1,2
 
 
+
 -- Total Cases vs Total Deaths
+--create new column "DeathPercentage" from total_deaths / total_cases * 100.  where the location includes united states
+
 
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
@@ -27,8 +30,12 @@ and continent is not null
 order by 1,2
 
 
+
 -- Total Cases vs Population
+--create new column "PercentPopulationInfected" from total_cases / population * 100.  again where the location includes united states
 -- Shows what percentage of population infected with Covid
+
+
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
@@ -37,6 +44,9 @@ order by 1,2
 
 
 -- Countries with Highest Infection Rate compared to Population
+--create new column "HighestInfectionCount" from highest total cases.  Also create new column "PercentPopulationInfected" highest(total_cases / population * 100 
+
+
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths
@@ -45,7 +55,11 @@ Group by Location, Population
 order by PercentPopulationInfected desc
 
 
+
 -- Countries with Highest Death Count per Population
+----create new column "TotalDeathCount" from highest(total deaths).  Cast as int data type to make calculation
+--by location
+
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
@@ -59,6 +73,9 @@ order by TotalDeathCount desc
 -- BREAKING THINGS DOWN BY CONTINENT
 
 -- Showing contintents with the highest death count per population
+--create new column "TotalDeathCount" from highest(total deaths).  Cast as int data type to make calculation
+--by continent
+
 
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
@@ -70,6 +87,9 @@ order by TotalDeathCount desc
 
 
 -- GLOBAL NUMBERS
+--new column "total_cases" taking the sum of new_cases. new column "total_deaths" taking sum new_deaths.  Must cast as int to make calculation
+--new column "DeathPercentage" new_deaths / the sum of new_cases. Must cast as int to make calculation(new_deaths)
+
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
@@ -82,6 +102,10 @@ order by 1,2
 
 -- Total Population vs Vaccinations
 -- Percentage of Population that has recieved at least one Covid Vaccine
+--join coviddeaths with covidvaccinations table on location column and date column
+--create new column "RollingPeopleVaccinated". sum and convert new_vaccinations to int.partition by location
+
+
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
@@ -94,7 +118,11 @@ where dea.continent is not null
 order by 2,3
 
 
+
 -- Using CTE to perform Calculation on Partition By in previous query
+
+
+
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
@@ -112,9 +140,6 @@ where dea.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100
 From PopvsVac
 
-
-
--- Using Temp Table to perform Calculation on Partition By in previous query
 
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -145,6 +170,8 @@ From #PercentPopulationVaccinated
 
 
 -- Creating View to store data for later visualizations
+
+
 
 Create View PercentPopulationVaccinated as
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
